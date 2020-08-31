716e7d99a30b26012e5711fb2f33bea71a7bc5ab
Guido Kraemer <gdkrmr@users.noreply.github.com>
2020-08-31 09:38:46 +0200

    julia 1.5 compat (#32)
    julia 1.5 compat, make the server multi-root, match vscode 1.0.5, add symbol server store path

--- lsp-julia.el
+++ lsp-julia.el
@@ -9,7 +9,7 @@
 ;;             Guido Kraemer <gdkrmr@users.noreply.github.com>
 ;; Keywords: languages, tools
 ;; Version: 0.2.0
-;; Package-Requires: ((emacs "25.1") (lsp-mode "6.0") (julia-mode "0.3"))
+;; Package-Requires: ((emacs "25.1") (lsp-mode "6.3") (julia-mode "0.3"))
 ;; Keywords: languages, tools
 ;; URL: https://github.com/non-Jedi/lsp-julia
 
@@ -83,6 +83,15 @@ Set to nil if you want to use the globally installed versions."
   :type '(repeat (string :tag "argument"))
   :group 'lsp-julia)
 
+(defcustom lsp-julia-symbol-server-store-path "~/.julia/symbolstorev2-lsp-julia"
+  "The cache directory for `SymbolServer.jl'."
+  :type 'directory
+  ;; :initialize (lambda (sym expr)
+  ;;               (print expr)
+  ;;               (when expr (make-directory expr t) expr))
+  :group 'lsp-julia)
+
+
 (defcustom lsp-julia-timeout 30
   "Time before symbol `lsp-mode' should assume julia just ain't gonna start."
   :type 'number
@@ -93,27 +102,184 @@ Set to nil if you want to use the globally installed versions."
   :type 'string
   :group 'lsp-julia)
 
+;;; Workspace options
+(defcustom lsp-julia-format-indent 4
+  "Indent size for formatting."
+  :type 'integer
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-format-indents t
+  "Format file indents."
+  :type 'boolean
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-format-ops t
+  "Format whitespace around operators."
+  :type 'boolean
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-format-tuples t
+  "Format tuples."
+  :type 'boolean
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-format-curly t
+  "Format braces."
+  :type 'boolean
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-format-calls t
+  "Format function calls."
+  :type 'boolean
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-format-iterops t
+  "Format loop iterators."
+  :type 'boolean
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-format-comments t
+  "Format comments."
+  :type 'boolean
+  :group 'lsp-julia)
+(defcustom lsp-julia-format-docs t
+  "Format inline documentation."
+  :type 'boolean
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-format-kw t
+  "Remove spaces around = in function keywords."
+  :type 'boolean
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-lint-run t
+  "Run the linter on active files."
+  :type 'boolean
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-lint-missingrefs "all"
+  "Highlight unknown symbols. The `symbols` option will not mark
+unknown fields."
+  :type 'string
+  :options '("none" "symbols" "all")
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-lint-call t
+  "This compares call signatures against all known methods for
+the called function. Calls with too many or too few arguments, or
+unknown keyword parameters are highlighted."
+  :type 'boolean
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-lint-iter t
+  "Check iterator syntax of loops. Will identify, for example,
+attempts to iterate over single values."
+  :type 'boolean
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-lint-constif t
+  "Check for constant conditionals in if statements that result
+in branches never being reached.."
+  :type 'boolean
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-lint-lazy t
+  "Check for deterministic lazy boolean operators."
+  :type 'boolean
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-lint-datadecl t
+  "Check variables used in type declarations are datatypes."
+  :type 'boolean
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-lint-typeparam t
+  "Check parameters declared in `where` statements or datatype
+declarations are used."
+  :type 'boolean
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-lint-modname t
+  "Check submodule names do not shadow their parent's name."
+  :type 'boolean
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-lint-pirates t
+  "Check for type piracy - the overloading of external functions
+with methods specified for external datatypes. 'External' here
+refers to imported code."
+  :type 'boolean
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-lint-useoffuncargs t
+  "Check that all declared arguments are used within the function
+body."
+  :type 'boolean
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-lint-nothingcomp t
+  "Check for use of `==` rather than `===` when comparing against
+`nothing`."
+  :type 'boolean
+  :group 'lsp-julia)
+
+(lsp-register-custom-settings
+ '(("julia.format.indent"      lsp-julia-format-indent)
+   ("julia.format.indents"     lsp-julia-format-indents     t)
+   ("julia.format.ops"         lsp-julia-format-ops         t)
+   ("julia.format.tuples"      lsp-julia-format-tuples      t)
+   ("julia.format.curly"       lsp-julia-format-curly       t)
+   ("julia.format.calls"       lsp-julia-format-calls       t)
+   ("julia.format.iterOps"     lsp-julia-format-iterops     t)
+   ("julia.format.comments"    lsp-julia-format-comments    t)
+   ("julia.format.docs"        lsp-julia-format-docs        t)
+   ("julia.format.kw"          lsp-julia-format-kw          t)
+   ("julia.lint.run"           lsp-julia-lint-run           t)
+   ("julia.lint.missingrefs"   lsp-julia-lint-missingrefs)
+   ("julia.lint.call"          lsp-julia-lint-call          t)
+   ("julia.lint.iter"          lsp-julia-lint-iter          t)
+   ("julia.lint.constif"       lsp-julia-lint-constif       t)
+   ("julia.lint.lazyif"        lsp-julia-lint-lazy          t)
+   ("julia.lint.datadecl"      lsp-julia-lint-datadecl      t)
+   ("julia.lint.typeparam"     lsp-julia-lint-typeparam     t)
+   ("julia.lint.modname"       lsp-julia-lint-modname       t)
+   ("julia.lint.pirates"       lsp-julia-lint-pirates       t)
+   ("julia.lint.useoffuncargs" lsp-julia-lint-useoffuncargs t)
+   ("julia.lint.nothingcomp"   lsp-julia-lint-nothingcomp   t)))
+
+;;; lsp-julia related functions setup
 (defun lsp-julia--get-root ()
   "Get the (Julia) project root directory of the current file."
-  (let ((dir (locate-dominating-file default-directory "Project.toml")))
-    (if dir (expand-file-name dir)
-      (expand-file-name lsp-julia-default-environment))))
+  (let* ((dir (locate-dominating-file default-directory "Project.toml"))
+         (dir2 (if dir dir lsp-julia-default-environment)))
+    (concat "\"" (expand-file-name dir2) "\"")))
 
 (defun lsp-julia--get-depot-path ()
   "Get the (Julia) depot path."
-  (let ((dp (getenv "JULIA_DEPOT_PATH")))
-    (if dp dp lsp-julia-default-depot)))
+  (let* ((dp (getenv "JULIA_DEPOT_PATH"))
+         (dp2 (if dp dp lsp-julia-default-depot)))
+    (concat "\"" dp2 "\"")))
+
+(defun lsp-julia--symbol-server-store-path-to-jl ()
+  "Convert the variable `lsp-julia-symbol-server-store-path' to a
+  string or \"nothing\" if `nil'"
+  (if lsp-julia-symbol-server-store-path
+      (let ((sssp (expand-file-name lsp-julia-symbol-server-store-path)))
+        (make-directory sssp t)
+        (concat "\"" sssp "\""))
+    "nothing"))
 
 (defun lsp-julia--rls-command ()
   "The command to lauch the Julia Language Server."
   `(,lsp-julia-command
     ,@lsp-julia-flags
-    ,(concat "-e using LanguageServer, Sockets, SymbolServer;"
+    ,(concat "-e using InteractiveUtils, Sockets, SymbolServer, LanguageServer;"
              " server = LanguageServer.LanguageServerInstance("
-             " stdin, stdout, false,"
-             " \"" (lsp-julia--get-root) "\","
-             " \"" (lsp-julia--get-depot-path) "\");"
-             " server.runlinter = true;"
+             " stdin, stdout,"
+             (lsp-julia--get-root) ","
+             (lsp-julia--get-depot-path) ","
+             " nothing, "
+             (lsp-julia--symbol-server-store-path-to-jl) ");"
              " run(server);")))
 
 (defconst lsp-julia--handlers
@@ -125,7 +291,8 @@ Set to nil if you want to use the globally installed versions."
 (lsp-register-client
  (make-lsp-client :new-connection (lsp-stdio-connection 'lsp-julia--rls-command)
                   :major-modes '(julia-mode ess-julia-mode)
-                  :server-id 'julia-ls))
+                  :server-id 'julia-ls
+                  :multi-root t))
 
 (provide 'lsp-julia)
 ;;; lsp-julia.el ends here
